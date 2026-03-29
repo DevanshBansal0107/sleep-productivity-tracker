d7fc71622b6ac28dae76147fbc7d2e5c81913086
DevanshBansal0107 <devanshbansal.code@gmail.com>
2026-03-29 23:29:25 +0530

    Add model loading and prediction functions

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,8 @@
+import pickle
 
+def load_model():
+    with open("models/trained_model.pkl", "rb") as f:
+        return pickle.load(f)
+
+def predict(model, sleep, study, screen, stress):
+    return model.predict([[sleep, study, screen, stress]])[0]
